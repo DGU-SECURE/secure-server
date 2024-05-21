3bff1455304ff04dd7016351a230a67fadd0404e
prefer52 <tjsgh151@naver.com>
2024-05-21 17:24:26 +0900

    init: 프로젝트 초기 설정

---
+++ src/main/kotlin/edu/dongguk/nusinsa/NusinsaApplication.kt
@@ -0,0 +1,11 @@
+package edu.dongguk.nusinsa
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NusinsaApplication
+
+fun main(args: Array<String>) {
+	runApplication<NusinsaApplication>(*args)
+}
